87857fd4629bf790c25f69cf155983abfa2dab6a
shatiku <kuzu4649@hotmail.co.jp>
2022-08-05 19:44:26 +0900

    Refactoring

--- ddsk.py
+++ ddsk.py
@@ -2,7 +2,7 @@ import random
 
 def main():
     str_ddsk = ["ドド","スコ"]
-    str_end = (str_ddsk[0] + str_ddsk[1] * 3) * 3
+    str_end = (str_ddsk[0] + (str_ddsk[1] * 3)) * 3
     str_love = "LOVE注入♡"
     str_buf = ""
     str_random = ""
